980abbbddd9e18a8b79ed0eeea6e2c20bca330e9
Pramod <giripramodsunway789@gmail.com>
2023-12-19 22:57:22 +0545

    Javascript practice Session

---
+++ sandbox.js
@@ -0,0 +1,53 @@
+// alert("HELLO WORLD");
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// To create variables.
+// let age = 25;
+// let year = 2024;
+// // log things to console.
+// console.log(age, year);
+
+// age = 50;
+// console.log(age);
+
+// // To create constant Variables.
+// const points = 10000;
+// console.log(points);
+
+// // Older way to create variables using var.
+// var score = 70;
+// console.log(score);
+
+// Javascript Data types
+// 1. Strings
+console.log("Hello World");
+let email = "pg016742@gmail.com";
+console.log(email);
+
+// Strings Concatenations (joining)
+// let firstname = 'Pramod';
+// let lastname ='Giri';
+// let fullname = firstname + ' ' + lastname;
+// console.log(fullname);
+
+// getting characters.
+// console.log(fullname[1]);
+
+// string length
+// console.log(fullname.length);
+
+// string methods
+// console.log(fullname.toUpperCase());
+// let result = fullname.toLowerCase();
+// console.log(result, fullname);
+// let index = email.indexOf('0');
+// console.log(index);
+
+// Common string methods
+// let result = email.lastIndexOf('0');
+// let result = email.slice(1,5);
+// let result = email.substring(0,10);
+let result = email.replace('p','a');
+console.log(result);
